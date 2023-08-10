1e3c2f1bd3c394b0183445a8dbbd7fb22c55f838
bethimanideep <bethimanideep@gmail.com>
2023-08-10 14:47:12 +0530

    upd

--- crudApp/settings.py
+++ crudApp/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-z9h1rc$t0#7=7^7!x6$)=xm_db1xf#c2+y7*(&1&c849-j++e9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
-
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 # Application definition
 
@@ -74,14 +73,6 @@ WSGI_APPLICATION = 'crudApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -122,3 +113,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
